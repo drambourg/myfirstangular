6e3b5a425a9a4ae8bf114cad7646afddfe207bc4
Damien Rambourg <damien.rambourg@gmail.com>
2019-04-07 10:26:18 +0200

    Mise à jour des variables d'Application Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = 'Damien';
+  avis = 'super';
+  objdate = new Date();
+  date = this.objdate.getDate() + "/" + this.objdate.getMonth() + "/" + this.objdate.getFullYear();
 }
