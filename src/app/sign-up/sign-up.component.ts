c4114786cb3e3e54c548e7ad04a581dc51216d62
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-05 10:35:23 +0200

    Fix conflict merge

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Email } from '../email';
-=======
->>>>>>> 5f3bf94b9738efcbeb8b0c83511b624466a2c35d
 
 @Component({
   selector: 'app-sign-up',
@@ -10,13 +7,10 @@ import { Email } from '../email';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-<<<<<<< HEAD
-  email:Email= {
-    mail:'Superman@gmail.com'
+
+  email: Email = {
+    mail: 'Superman@gmail.com'
   }
-=======
-  email=""
->>>>>>> 5f3bf94b9738efcbeb8b0c83511b624466a2c35d
   constructor() { }
 
   ngOnInit() {
