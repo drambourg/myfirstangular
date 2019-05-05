a9cb53ace03626621a5d6cfde5347f20755afe86
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-05 11:32:14 +0200

    Mise en place du composant User profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserProfilComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-profil/user-profil.component.ts
+++ src/app/user-profil/user-profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-profil',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profil.component.css']
 })
 export class UserProfilComponent implements OnInit {
+  public show: boolean;
+  public buttonName: any = 'Afficher l\'âge';
+    user: User = {
+      name : 'Doe',
+      firstName : 'John',
+      age : 25,
+      quote : '',
+      photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleAge() {
+    this.show = !this.show;
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.show) {
+      this.buttonName = 'Afficher l\'âge';
+    } else {
+      this.buttonName = 'Masque l\'âge';
+    }
+  }
 }
